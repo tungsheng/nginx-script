50b38e15d29575b6d384d9786bf4571a1a245150
Tony Lee <tungsheng@gmail.com>
2018-06-30 02:13:12 +0800

    add certbot script

---
+++ certbot.sh
@@ -0,0 +1,20 @@
+sudo apt-get update
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install -y python-certbot-nginx
+sudo certbot --nginx
+
+# You can check renewal works using:
+# sudo certbot renew --dry-run
+# You can also check what certificates exist using:
+# sudo certbot certificates
+
+# http/2
+# change:
+# listen [::]:443 ssl ipv6only=on;
+# listen 443 ssl;
+
+# to:
+# listen [::]:443 ssl http2 ipv6only=on;
+# listen 443 ssl http2;
+# gzip off;
