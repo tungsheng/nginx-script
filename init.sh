b9572b4e0244a3f98edadaa84bca53cc585aaaad
Tony Lee <tungsheng@gmail.com>
2018-06-30 12:31:41 +0800

    update init process, create file instead cp

--- init.sh
+++ init.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+
 function getDomain() {
+    echo -e "\n\n"
     read -e -p "Enter a domain (e.g. domain.com) " -r domain
     if [[ "${#domain}" -lt 1 ]]; then
       echo " >> Please enter a valid domain!"
       readDomain
     fi
+    echo -e "\n\n"
 }
 
 # install nginx
@@ -20,14 +24,36 @@ fi
 mkdir -p $dir
 
 # init www
-wwwFile="/var/www/$domain"
-cp index.html $wwwFile
+wwwDIR="/var/www/$domain"
+if [ -e "/var/www/$domain/index.html" ]; then
+    rm -f "/var/www/$domain/index.html"
+else
+cp index.html $wwwDIR
 
 # create conf file
+availableDIR="/etc/nginx/sites-available/"
+enabledDIR="/etc/nginx/sites-enabled/"
 confFile="$domain.conf"
-cp $confFile /etc/nginx/sites-available
+if [ -e "$availableDIR$conffile" ]; then
+    rm -f "$availableDIR$conffile"
+else
+cat <<EOF >"$availableDIR$confFile"
+server {
+       listen 80 default_server;
+       listen [::]:80 default_server ipv6only=on;
 
-ln -s /etc/nginx/sites-available/$confFile /etc/nginx/sites-enabled/$confFile
+       server_name $domain www.domain;
+
+       root /var/www/$domain;
+       index index.html;
+
+       location / {
+               try_files $uri $uri/ =404;
+       }
+}
+EOF
+
+ln -s "$availableDIR$confFile" "$enabledDIR$confFile"
 
 # start nginx
 nginx -t
