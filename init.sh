4bfc536d72d8e333737b11da5efe6eb4d4c79c00
Tony Lee <tungsheng@gmail.com>
2018-06-30 12:04:39 +0800

    update init process

--- init.sh
+++ init.sh
@@ -1,9 +1,34 @@
+function getDomain() {
+    read -e -p "Enter a domain (e.g. domain.com) " -r domain
+    if [[ "${#domain}" -lt 1 ]]; then
+      echo " >> Please enter a valid domain!"
+      readDomain
+    fi
+}
+
+# install nginx
 apt-get install -y nginx
-mkdir -p "/var/www/$domain"
 
-cp index.html /var/www/mydomain.com
-cp domain.com.conf /etc/nginx/sites-available
-ln -s /etc/nginx/sites-available/mydomain.com.conf /etc/nginx/sites-enabled/mydomain.com.conf
+# get domain name
+getDomain
+dir="/var/www/$domain"
+if [ -d "$dir" ]; then
+  # dir exists.
+  rm -rf $dir
+fi
 
+mkdir -p $dir
+
+# init www
+wwwFile="/var/www/$domain"
+cp index.html $wwwFile
+
+# create conf file
+confFile="$domain.conf"
+cp $confFile /etc/nginx/sites-available
+
+ln -s /etc/nginx/sites-available/$confFile /etc/nginx/sites-enabled/$confFile
+
+# start nginx
 nginx -t
 systemctl restart nginx
