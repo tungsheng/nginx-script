11e472f0f540ff96a374243bd822e1f813cba99e
Tony Lee <tungsheng@gmail.com>
2018-10-04 13:28:59 +0800

    add service

--- init.sh
+++ init.sh
@@ -15,11 +15,37 @@ curl -fsSL https://nginx.org/keys/nginx_signing.key | apt-key add -
 echo "deb https://nginx.org/packages/mainline/debian/ $(lsb_release -cs) nginx\n" | tee -a /etc/apt/sources.list.d/nginx.list
 echo "deb-src https://nginx.org/packages/mainline/debian/ $(lsb_release -cs) nginx" | tee -a /etc/apt/sources.list.d/nginx.list
 
-
 # install nginx
 apt-get remove nginx-common
 apt-get update
 apt-get install -y nginx
+
+# add Nginx service
+echo -ne "Adding Nginx service...\n"
+touch /lib/systemd/system/nginx.service
+cat <<EOT >> /lib/systemd/system/nginx.service
+[Unit]
+Description=The NGINX HTTP and reverse proxy server
+After=syslog.target network.target remote-fs.target nss-lookup.target
+
+[Service]
+Type=forking
+PIDFile=/var/run/nginx.pid
+ExecStartPre=/usr/bin/nginx -t
+ExecStart=/usr/bin/nginx
+ExecReload=/usr/sbin/nginx -s reload
+ExecStop=/bin/kill -s QUIT $MAINPID
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+echo -ne "Enabling Nginx...\n"
+systemctl enable nginx
+
+echo -ne "Starting Nginx...\n"
+systemctl start nginx
 
 # get domain name
 getDomain
