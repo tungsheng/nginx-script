5b77375c19676bd0d22ab2d50ca1b986a18cdeaa
Tony Lee <tungsheng@gmail.com>
2018-10-12 01:16:34 +0800

    addsite when init

--- init.sh
+++ init.sh
@@ -4,8 +4,8 @@
 version=1.15.5
 
 # Install dependancies
-apt-get update
-apt-get install -y \
+sudo apt-get update
+sudo apt-get install -y \
   build-essential \
   libpcre3 \
   libpcre3-dev \
@@ -15,12 +15,12 @@ apt-get install -y \
   zlib1g-dev
 
 # Download source
-wget http://nginx.org/download/nginx-${version}.tar.gz
-tar -zxvf nginx-${version}.tar.gz
+sudo wget http://nginx.org/download/nginx-${version}.tar.gz
+sudo tar -zxvf nginx-${version}.tar.gz
 cd nginx-${version}/
 
 # Compile source
-./configure \
+sudo ./configure \
   --without-http_autoindex_module \
   --sbin-path=/usr/bin/nginx \
   --conf-path=/etc/nginx/nginx.conf \
@@ -32,29 +32,29 @@ cd nginx-${version}/
   --with-http_v2_module
 
 # Install source
-make
-make install
+sudo make
+sudo make install
 
 # Firewall allows nginx http
 nginxHTTP=/etc/ufw/applications.d/nginx-http
 nginxHTTPS=/etc/ufw/applications.d/nginx-https
 nginxFULL=/etc/ufw/applications.d/nginx-full
-touch nginxHTTP
-touch nginxHTTPS
-touch nginxFULL
-cat <<EOT > $nginxHTTP
+sudo touch nginxHTTP
+sudo touch nginxHTTPS
+sudo touch nginxFULL
+sudo cat <<EOT > $nginxHTTP
 [Nginx HTTP]
 title=Web Server (HTTP)
 description=for serving web
 ports=80/tcp
 EOT
-cat <<EOT > $nginxHTTPS
+sudo cat <<EOT > $nginxHTTPS
 [Nginx HTTPS]
 title=Web Server (HTTPS)
 description=for serving web
 ports=443/tcp
 EOT
-cat <<EOT > $nginxHTTPS
+sudo cat <<EOT > $nginxHTTPS
 [Nginx Full]
 title=Web Server (HTTP and HTTPS)
 description=for serving web
@@ -63,7 +63,10 @@ EOT
 sudo ufw allow 'Nginx HTTP'
 
 # Update nginx.conf
-cp -f $HOME/nginx-script/nginx.example.conf /etc/nginx/nginx.conf
+sudo cp -f $HOME/nginx-script/nginx.example.conf /etc/nginx/nginx.conf
+
+# Add site
+sudo source $HOME/nginx-script/addsite.sh
 
 # Add Nginx service
 echo -ne "Adding Nginx service...\n"
